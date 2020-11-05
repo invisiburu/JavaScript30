1f5dd7d726b2a1c06bd7fc91938abaaaf8dc830a
Maksym Nesterov <maxym.nes.dev@gmail.com>
2020-11-05 22:45:23 +0200

    do 7

---
+++ 07 - Array Cardio Day 2/script.js
@@ -0,0 +1,36 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+const getAge = person => new Date().getFullYear() - person.year
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
+console.log(people.some(el => getAge(el) >= 19))
+console.log(people.every(el => getAge(el) >= 19))
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+console.log(comments.find(el => el.id === 823423))
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const idx = comments.findIndex(el => el.id === 823423)
+delete comments[idx]
+console.table([...comments.slice(0, idx), ...comments.slice(idx + 1)])
